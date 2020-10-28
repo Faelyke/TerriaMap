999fc4593ba5a40f1fae4e3948106d3bf3ac142a
AidenS <aidensoedjarwo@gmail.com>
2020-10-29 10:41:56 +1100

    Redirect request to rainviewer.

--- cesium_data/weather_proxy.py
+++ cesium_data/weather_proxy.py
@@ -10,19 +10,23 @@ from flask_cors import CORS
 app = Flask(__name__.split('.')[0])
 CORS(app)
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 CHUNK_SIZE = 1024
-LOG = logging.getLogger("app.py")
+#LOG = logging.getLogger("app.py")
 
 # this will need to be changed
-address = ('127.0.0.1', 80)
+address = ('0.0.0.0', 3002)
+live=True
 
 @app.route('/radar/<path:url>', methods=["GET", "POST"])
 def root(url):
-    if int(address[1] ) != 80:
+    if int(address[1] ) != 80 and not live:
         _url = request.url.split(f"{address[0]}:{address[1]}")
+    elif live:
+        _url = request.url.split("radar.iorama.geosynergy.com.au")
     else:
         _url = request.url.split(address[0])
+    #print(_url)
     if 'favicon' not in request.url:
         keys = _url[1].split("/")
         z = int(keys[2])
@@ -38,9 +42,10 @@ def root(url):
 
         # https://tilecache.rainviewer.com/v2/radar/1603861200/512/2/2/1/1/1_1.png
         
-        r = requests.get(f"https://tilecache.rainviewer.com/v2/radar/{timestamp}/512/{z}/{x}/{y}/1/1_1.png")
+        #r = requests.get(f"https://tilecache.rainviewer.com/v2/radar/{timestamp}/512/{z}/{x}/{y}/1/1_1.png")
+        return redirect(f"https://tilecache.rainviewer.com/v2/radar/{timestamp}/512/{z}/{x}/{y}/1/1_1.png", code=302)
         #print(r.content)
-        return Response(r.content, mimetype="image/png")
+        #return Response(r.content, mimetype="image/png")
 
     return Response("Nothing sent")
 
